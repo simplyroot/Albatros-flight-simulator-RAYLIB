c263c63e9df54118008c0b787d4ed1b9bd27ed9d
Oguz Kaynak <oguzahmetkaynak@hotmail.com>
2026-06-15 19:05:01 +0300

    Add window, game loop and dt-based movement

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+//g++ -Wall -Wextra src/main.cpp -o albatros.exe -lraylib -lopengl32 -lgdi32 -lwinmm
+#include <raylib.h>
+
+
+
+
+int main(){
+
+InitWindow(1280,720,"Albatros");
+SetTargetFPS(165);
+float circleX = -20.0f; 
+
+while(!WindowShouldClose()){
+    float dt = GetFrameTime();
+    
+    circleX += 120.0f * dt;
+    if (circleX > 1300.0f) {     
+        circleX = -20.0f;       
+    }
+    
+    BeginDrawing();
+    ClearBackground(Color{15, 18, 26, 255});
+    DrawCircle((int)circleX,360,20,PURPLE);
+
+    DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, RAYWHITE);
+    DrawText(TextFormat("Time: %.1f", GetTime()), 10, 30, 20, RAYWHITE);
+
+    EndDrawing();
+
+
+
+
+
+
+}
+
+CloseWindow();
+
+}
